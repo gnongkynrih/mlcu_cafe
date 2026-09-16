9709d17949bb03009f143c7fd158e809af1abf67
Gordon <kynsai@gmail.com>
2026-09-16 12:57:34 +0530

    16.09.2026

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,4 +10,15 @@ class Category extends Model
 {
     //since we are using the soft deletes, we need to tell the model to use the soft deletes
     use SoftDeletes, HasFactory;
+
+    //allow mass assignment to all fields except id
+    protected $guarded = ['id'];
+
+    protected function casts(): array
+    {
+        return [
+            'sort_order' => 'integer',
+            'is_active' => 'boolean'
+        ];
+    }
 }
--- resources/views/pages/admin/⚡category-management.blade.php
+++ resources/views/pages/admin/⚡category-management.blade.php
@@ -6,10 +6,28 @@ use App\Models\Category;
 new class extends Component
 {
     public $categories= []; //to store the categories
+    public $name;
+    public $description;
+    public $showModal;
+    public $selectedCategoryId;
 
+    public function rules(){
+        return [
+            'name' => 'required|string|max:25|min:3|unique:categories,name',
+            'description' => 'nullable|string|max:255',
+        ];
+    }
+    public function messages(){
+        return [
+            'name.required' =>'Category Name is required',
+            'name.min' =>'Category Name must be atleast 3 characters',
+            'name.unique' =>'Category Name already exists',
+        ];
+    }
     //mount is a lifecycle method that is called when the component is loaded
     public function mount()
     {
+        $this->showModal = false;
         //select * from categories
         // $this->categories = Category::all();
 
@@ -18,6 +36,65 @@ new class extends Component
             ->get();
     }
     
+    public function create(){
+        $this->selectedCategoryId = null;
+        $this->name = '';
+        $this->description = '';
+        $this->showModal = true;
+    }
+    public function save(){
+
+        //validate the input
+        $this->validate();
+
+        //insert into categories () values ()
+        Category::create([
+            'name' => $this->name,
+            'description' => $this->description,
+        ]);
+        
+        //reset the form
+        $this->name = '';
+        $this->description = '';
+        
+        //refresh the categories
+        $this->categories = Category::orderBy('sort_order', 'asc')->get();
+    }
+
+
+    //pass the id of the category
+    public function show($id){
+        $this->selectedCategoryId = $id;
+        //select * from categories where id = $id
+        $category = Category::find($id); //find searches by id
+        $this->name = $category->name;
+        $this->description = $category->description;
+        $this->showModal = true;
+    }
+
+    public function update(){
+        //selecting the category base on id and then update the data
+        Category::where('id', $this->selectedCategoryId)->update([
+            'name' => $this->name,
+            'description' => $this->description,
+        ]);
+        
+        //refresh the categories
+        $this->categories = Category::orderBy('sort_order', 'asc')->get();
+        
+        //close the modal
+        $this->showModal = false;
+        $this->selectedCategoryId = null;
+    }
+    public function updateStatus($id): void
+    {
+        //findOrFail - if the id does not exist show 404 page
+        $category = Category::findOrFail($id);
+        $category->is_active = ! $category->is_active;
+        $category->save();
+
+        $this->categories = Category::orderBy('sort_order', 'asc')->get();
+    }
 }
 ?>
 
@@ -48,9 +125,8 @@ new class extends Component
         </div>
 
        <div class="flex justify-end mb-2">
-        <flux:modal.trigger name="add-category"> 
-            <flux:button variant="primary"class="bg-purple-700 text-white hover:bg-indigo-800" icon="plus">New Category</flux:button>
-        </flux:modal.trigger>
+       
+            <flux:button wire:click="create" variant="primary"class="bg-purple-700 text-white hover:bg-indigo-800" icon="plus">New Category</flux:button>
         </div>
         {{-- Main Card --}}
         <div class="bg-white rounded-2xl shadow-md border border-r-red-200 overflow-hidden">
@@ -59,15 +135,16 @@ new class extends Component
             <div class="px-6 py-4 border-b border-slate-100 bg-indigo-500">
                 <div class="grid grid-cols-12 gap-4 text-xs font-semibold  uppercase tracking-wider">
                     <div class="col-span-1">#</div>
-                    <div class="col-span-7">Category Name</div>
+                    <div class="col-span-6">Category Name</div>
                     <div class="col-span-2 text-center">Sort Order</div>
                     <div class="col-span-2 text-right">Status</div>
+                    <div class="col-span-1 text-center">Actions</div>
                 </div>
             </div>
 
             {{-- Categories List --}}
             <div class="divide-y divide-slate-100">
-                @forelse ($categories as $category)
+                @forelse ($categories as $index => $category)
                     <div 
                         x-data="{ hovered: false }"
                         @mouseenter="hovered = true"
@@ -83,7 +160,7 @@ new class extends Component
                         </div>
 
                         {{-- Name --}}
-                        <div class="col-span-7">
+                        <div class="col-span-6">
                             <div class="flex items-center gap-3">
                                 <div class="w-2 h-2 rounded-full bg-indigo-500"></div>
                                 <span class="font-medium text-slate-800">
@@ -94,9 +171,7 @@ new class extends Component
 
                         {{-- Sort Order --}}
                         <div class="col-span-2 text-center">
-                            <span class="inline-flex items-center px-2.5 py-1 rounded-md text-sm font-medium bg-slate-100 text-slate-700">
-                                {{ $category->sort_order }}
-                            </span>
+                            <flux:input type="number" wire:model.blur="category.{{ $index }}.sort_order" />
                         </div>
                         @php
                             $status = $category->is_active;
@@ -107,12 +182,34 @@ new class extends Component
                         {{-- Status Badge --}}
                         
                         <div class="col-span-2 text-right">
-                            <span class="inline-flex items-center gap-1.5 px-2.5 py-1 rounded-full text-xs font-medium {{ $statusClass }}">
+                            <flux:field variant="inline">
+                                <flux:label @class([
+                                    'text-emerald-600!' => $category->is_active,
+                                    'text-red-600!' => ! $category->is_active,
+                                ])>
+                                    {{ $statusText }}
+                                </flux:label>
+
+                                <flux:switch
+                                    wire:click="updateStatus({{ $category->id }})"
+                                    :checked="$category->is_active"
+                                    class="bg-red-400! data-checked:bg-emerald-500! data-checked:border-emerald-500!"
+                                />
+
+                                <flux:error name="is_active" />
+                            </flux:field>
+                            <!-- <span class="inline-flex items-center gap-1.5 px-2.5 py-1 rounded-full text-xs font-medium {{ $statusClass }}">
                                 <span class="w-1.5 h-1.5 rounded-full {{ $statusDotClass }}">
 
                                 </span>
                                  {{ $statusText }} 
-                            </span>
+                            </span> -->
+                        </div>
+                        <div class="col-span-1 flex">
+                            
+                            <flux:icon.pencil-square color="green" wire:click="show({{ $category->id }})" />
+                            
+                            <flux:icon.trash color="red" />
                         </div>
                     </div>
                 @empty
@@ -137,18 +234,22 @@ new class extends Component
     </div>
 
     <!-- Modal form -->
-    <flux:modal name="add-category" class="md:w-96 bg-white ">
+    <flux:modal wire:model.self="showModal" name="add-category" class="md:w-96 bg-white ">
         <div class="space-y-6 ">
             <div>
                 <flux:heading size="lg">New Categories</flux:heading>
                 <flux:text class="mt-2">Create a new category for your menu.</flux:text>
             </div>
-            <flux:input label="Name" placeholder="Category name" />
-            <flux:input label="Description" placeholder="Category description" />
-            <div class="flex">
-                <flux:spacer />
-                <flux:button type="submit" variant="primary">Save changes</flux:button>
-            </div>
+            <form wire:submit="{{ $selectedCategoryId ? 'update' : 'save' }}">
+                <flux:input wire:model="name" label="Name" placeholder="Category name" />
+                <flux:input wire:model="description" label="Description" placeholder="Category description" />
+                <div class="flex items-center justify-between mt-4">
+                    <flux:modal.close>
+                        <flux:button type="button" variant="ghost">Cancel</flux:button>
+                    </flux:modal.close>
+                    <flux:button type="submit" variant="primary">{{ $selectedCategoryId ? 'Update' : 'Save' }}</flux:button>
+                </div>
+            </form>
         </div>
     </flux:modal>
 </div>
